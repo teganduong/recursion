5b6bf5f04cd9b870c3b5bb7893025108493ea240
Tegan Duong <teganduong@gmail.com>
2016-01-20 12:12:04 -0800

    Complete stringifyJSON.js to pass specs
    * Matches the result of calling JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  var arrayValues = [],
+			objKeys = [],
+			keyValues = [];
+
+  // Check for primitive types
+  if (typeof obj === 'boolean' || typeof obj === 'number' || obj === null)
+  	return '' + obj;
+  else if (typeof obj === 'string')
+  	return '"' + obj + '"';
+
+  // Check for Array
+  else if (Array.isArray(obj)) {
+  	if (obj.length === 0)
+  		return '[]';
+  	else {
+  		obj.forEach(function(item) {
+  			arrayValues.push(stringifyJSON(item));
+  		});
+  		return '[' + arrayValues + ']';
+  	}
+  }
+  // Check for Object
+  else if (obj instanceof Object) {
+  	objKeys = Object.keys(obj);
+  	objKeys.forEach(function(key) {
+  		var stringKey = '"' + key + '":';
+  		var keyVal = obj[key];
+  		if (typeof keyVal === undefined || keyVal instanceof Function)
+  			keyValues.push('');
+  		else if (typeof keyVal === 'string')
+  			keyValues.push(stringKey + '"' + keyVal + '"');
+  		else if (typeof keyVal === 'boolean' || typeof keyVal === 'number' || keyVal === null)
+  			keyValues.push(stringKey + keyVal);
+  		// Recursive case: check for nested objects
+  		else if (keyVal instanceof Object) {
+  			keyValues.push(stringKey + stringifyJSON(keyVal));
+  		}
+  	});
+  	return '{' + keyValues + '}';
+  }
 };
